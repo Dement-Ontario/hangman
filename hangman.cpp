3bb9bcbef0b2913a6f9f43f171bdf28a23801908
Joseph T <jdtoronto999@outlook.com>
2026-06-08 20:59:31 -0700

    showInfo, guessLetter, checkLetter, bug fixes

--- hangman.cpp
+++ hangman.cpp
@@ -18,24 +18,65 @@ class UI {
             "Right Leg"
         };
     public:
-        void showInfo(const char problem[], int livesLost, const char wrongLetters[]) {
-            cout << "ShowInfo not implemented.\n";
+        void showInfo(const char problem[], int pLength, int livesLost, const char wrongLetters[]) {
+            cout << hangmanStatus[livesLost] << "\n\n";
+            
+            if (wrongLetters[0] != ' ') {
+                cout << "Wrong Guesses: ";
+                for (int i = 0; i <= livesLost - 1; i++) {
+                    if (i == livesLost - 1) {
+                        cout << wrongLetters[i] << "\n";
+                    } else {
+                        cout << wrongLetters[i] << ", ";
+                    }
+                }
+            }
+
+            cout << "Problem: ";
+            for (int i = 0; i <= pLength - 1; i++) {
+                if (i == pLength - 1) {
+                    cout << problem[i] << "\n";
+                } else {
+                    cout << problem[i] << " ";
+                }
+            }
         }
 
-        void guessLetter() {
-            cout << "guessLetter not implemented.\n";
+        char guessLetter() {
+            char guess;
+            string guessString;
+            
+            cout << "Guess a letter: ";
+            while (guessString.length() != 1 || guessString == " ") {
+                cin >> guessString;
+
+                if (guessString.length() == 1) {
+                    guess = toupper(guessString[0]);
+                } else {
+                    cout << "\nInput too long. Guess a LETTER: ";
+                }
+            }
+            
+            return guess;
         }
 };
 
 class Hangman {
     private:
         UI ui;
-        string wordsList[4] = {"pickle", "wyrmwood", "alabaster", "a newt"};
+        string wordsList[6] = {
+            "PICKLE",
+            "WYRMWOOD",
+            "ALABASTER",
+            "A NEWT",
+            "THE VERY MODEL OF A MODERN MAJOR GENERAL",
+            "PORTAL_LOVER_253"
+        };
         string word;
     public:
         char* problem;
         int livesLost = 0;
-        char wrongLetters[6];
+        char wrongLetters[6] = {' ', ' ', ' ', ' ', ' ', ' '};
 
         Hangman() {
             int wordsListLength = sizeof(wordsList) / sizeof(wordsList[0]);
@@ -44,6 +85,7 @@ class Hangman {
             mt19937 gen(rd());
             uniform_int_distribution<> distr(0, wordsListLength - 1);
             word = wordsList[distr(gen)];
+            // word = wordsList[0]; // Use this for testing
 
             problem = new char[word.length()];
 
@@ -58,22 +100,36 @@ class Hangman {
 
         void runGame() {
             while (livesLost < 6 && !problemSolved()) {
-                ui.showInfo(problem, livesLost, wrongLetters);
-                ui.guessLetter();
-                livesLost++; // Remove this!
+                ui.showInfo(problem, word.length(), livesLost, wrongLetters);
+                char guess = ui.guessLetter();
+                checkLetter(guess);
             }
-
+            
+            ui.showInfo(problem, word.length(), livesLost, wrongLetters);
             if (livesLost >= 6) {
-                cout << "\nRan out of guesses. You lose!";
+                cout << "\nYou ran out of guesses. You lose!";
             } else if (problemSolved()) {
-                cout << "\nYou guess the whole word. You win!";
+                cout << "\nYou got all the letters right. You win!";
             }
 
             delete[] problem;
         }
 
         void checkLetter(char letter) {
+            bool hasLetter = false;
+            bool alreadyGuessed = find(begin(wrongLetters), end(wrongLetters), letter) != end(wrongLetters);
             
+            for (int i = 0; i <= word.length() - 1; i++) {
+                if (letter == word[i]) {
+                    problem[i] = letter;
+                    hasLetter = true;
+                }
+            }
+
+            if (!hasLetter && !alreadyGuessed) {
+                wrongLetters[livesLost] = letter;
+                livesLost++;
+            }
         }
         
         bool problemSolved() {
